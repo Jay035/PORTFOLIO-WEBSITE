ff5802fbe4aeceb402c23e2f6862d36093dc4b53
Chibuike Ukandu <jaydencolins@gmail.com>
2021-07-31 22:05:35 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('#hamburger');
+const inputField = document.querySelectorAll('input');
+const submitButton = document.getElementById('send');
+// const nameTitle = "Hello, I'm Chibuike Ukandu";
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+});
+
+for(const input of inputField){
+    input.addEventListener('click', () => {
+        input.style.borderBottomColor = '#fff';
+    })
+}
+
+// console.log(nameTitle.length)
