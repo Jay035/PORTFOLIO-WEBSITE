c0c64650881e591baf1cfcc1ba6a2f2e75fad404
Chibuike Ukandu <jaydencolins@gmail.com>
2021-10-18 10:47:49 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector('#hamburger');
 const inputField = document.querySelectorAll('input');
 const submitButton = document.getElementById('send');
-// const inputs = document.querySelectorAll('input');
 const textArea = document.querySelectorAll('.text');
 
 hamburger.addEventListener('click', () => {
@@ -20,14 +19,33 @@ for(const input of inputField){
     });
 };
 
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let firstName = id('firstName'),
+    lastName = id('lastName'),
+    email = id('email'),
+    textarea = id('message')
+    errorMsg = classes('error'),
+    successIcon = classes('success-icon'),
+    failureIcon = classes('failure-icon');
+
+
+
 // clear value after submit button is clicked
-for(const text of textArea){
-    submitButton.addEventListener('click', () =>{
-        text.value = '';
+    submitButton.addEventListener('click', (e) =>{
+        let engine = (id, serial, message) => {
+            id.value.trim() === ''
+            ? (errorMsg[serial].innerHTML = message, errorMsg[serial].style.marginBottom = '0.2em', id.style.borderBottom = '2px solid red', failureIcon[serial].style.opacity = '1', successIcon[serial].style.opacity = '0', e.returnValue = false)
+            : (errorMsg[serial].innerHTML = '', id.style.borderBottom = '2px solid green', failureIcon[serial].style.opacity = '0', successIcon[serial].style.opacity = '1', e.returnValue = true)
+        };
+        engine(firstName, 0, 'First name can\'t be blank');
+        engine(lastName, 1, 'Last name can\'t be blank');
+        engine(email, 2, 'Email can\'t be blank');
+        engine(textarea, 3, 'message can\'t be blank');
+        
+
+        
+
+        // text.value = '';
     });
-};
-
-
-// console.log(nameTitle.length)
-
-
